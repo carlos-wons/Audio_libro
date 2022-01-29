362b282e6c566497adc1f4eeb3d986ef2bcb80cc
Carlos Alberto Contreras ventura <72568840+carlos-wons@users.noreply.github.com>
2022-01-28 21:00:12 -0600

    Add files via upload

---
+++ app/src/main/java/com/example/audio_libro/MiIntentService.java
@@ -0,0 +1,39 @@
+package net.ivanvega.fragmentosdinamicos;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+public class MiIntentService extends IntentService {
+
+    /**
+     * @param name
+     * @deprecated
+     */
+    public MiIntentService(String name) {
+        super(name);
+    }
+
+    public MiIntentService(){super("MiIntentService");}
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        //Aqui debe se debe llevar acabo la tarea prolongada
+        // o de bloque
+        /*
+        try {
+            Thread.sleep(30000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
+
+        for(int i=0; i< intent.getIntExtra("arg1", 999999); i++){
+            Log.d("MiIntentService", "Iteracion: " + i);
+        }
+
+
+
+    }
+}
